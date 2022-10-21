5cd24876b2554a571f806720d247a1c1037943d8
Ndri45 <as.ndr82@gmail.com>
2022-10-21 08:37:39 +0700

    template halaman login & register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./containers/Login";
+import { Routes, Route, Link } from "react-router-dom";
+import Register from "./containers/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div>
+                            <h1>Home Page</h1>
+                            <Link to={"/login"}>to Login</Link>
+                        </div>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/containers/Login.js
@@ -0,0 +1,59 @@
+import { Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <section className="vh-100 gradient-custom" style={{ backgroundImage: "radial-gradient(circle,#b9090b,#141414)" }}>
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div className="card bg-dark text-white">
+                            <div className="card-body p-5 text-center">
+                                <div className="mb-md-5 mt-md-4 pb-5">
+                                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                    <p className="text-white-50 mb-5">Please enter your email and password!</p>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <label className="form-label" htmlFor="typeEmailX">
+                                            Email
+                                        </label>
+                                        <input type="email" id="typeEmailX" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <div className="form-outline form-white mb-4">
+                                        <label className="form-label" htmlFor="typePasswordX">
+                                            Password
+                                        </label>
+                                        <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                                    </div>
+
+                                    <p className="small mb-5 pb-lg-2">
+                                        <a className="text-white-50" href="#!">
+                                            Forgot password?
+                                        </a>
+                                    </p>
+
+                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">
+                                        Login
+                                    </button>
+                                </div>
+
+                                <div>
+                                    <p className="mb-0">
+                                        Don't have an account?{" "}
+                                        <Link to={"/register"} className="text-white-50 fw-bold">
+                                            Sign Up
+                                        </Link>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
