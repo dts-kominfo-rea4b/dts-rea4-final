74553b2132126595cf4e36f087d7dc6e0db1e9f8
Ndri45 <as.ndr82@gmail.com>
2022-10-24 21:47:45 +0700

    merge branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import request from "./api/Api";
+import app from "./api/Axios";
+import "./App.css";
+import NavbarComp from "./components/NavbarComp";
+import SliderComp from "./components/SliderComp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarComp />
+            <SliderComp url={app.getUri() + request.fetchNowPlaying} />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/SliderComp.js
+++ src/components/SliderComp.js
@@ -2,43 +2,34 @@ import React, { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import axios from "../api/Axios";
 
-
-const image_path = 'https://image.tmdb.org/t/p/original/';
+const image_path = "https://image.tmdb.org/t/p/original/";
 
 const SliderComp = (props) => {
     const [movies, setMovie] = useState([]);
 
     useEffect(() => {
-    async function fetchData() {
-        const request = await axios.get(props.url)
-        setMovie(request.data.results)
-    }
+        async function fetchData() {
+            const request = await axios.get(props.url);
 
-    fetchData()
-}, [])
+            setMovie(request.data.results);
+        }
+
+        fetchData();
+    }, []);
 
     return (
         <Carousel fade>
-        
-            { movies.slice(0,10).map(movie => {
+            {movies.slice(0, 10).map((movie, key) => {
                 return (
-                    <Carousel.Item>
-                    <img
-                    className="d-block w-100 "
-                    src={`${image_path}${movie.backdrop_path}`}
-                    alt={movie.original_title}
-                    style={{ height: '70vh' }}
-                    />
-                    <Carousel.Caption>
-                    <h3>{movie.original_title}</h3>
-                    <p>{movie.overview}</p>
-                    </Carousel.Caption>
+                    <Carousel.Item key={key}>
+                        <img className="d-block w-100 " src={`${image_path}${movie.backdrop_path}`} alt={movie.original_title} style={{ height: "70vh" }} />
+                        <Carousel.Caption>
+                            <h3>{movie.original_title}</h3>
+                            <p>{movie.overview}</p>
+                        </Carousel.Caption>
                     </Carousel.Item>
-                )
-            })
-            }
-        
-        
+                );
+            })}
         </Carousel>
     );
 };
