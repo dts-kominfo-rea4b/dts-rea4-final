40d3719c7385346f9b38c39ac84656b56f68ec4e
Hannan Nawawi <hnawawi939@gmail.com>
2022-10-25 03:57:34 +0000

    sort

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { ThemeProvider } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import MovieList from "./containers/MovieList";
+import Pricing from "./containers/Pricing";
+import Subscribed from "./containers/Subscribed";
 import theme from "./themes/theme";
 
-
-
-function App() {
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Navbar></Navbar>
-        <MovieList></MovieList>
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route
+            path="about"
+            element={<Box sx={{ mt: 10 }}>Halaman about</Box>}
+          />
+          <Route
+            path="indonesian"
+            element={<Box sx={{ mt: 10 }}>Halaman indonesian</Box>}
+          />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="subscribed/:plan" element={<Subscribed />} />
+        </Routes>
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,52 @@
-import { MovieFilter } from "@mui/icons-material";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import * as React from "react";
+import { MovieFilter } from '@mui/icons-material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
-const navItems = ["Indonesian", "Pricing", "About"];
+const navItems = [
+  { text: 'Indonesian', link: '/indonesian' },
+  { text: 'Pricing', link: '/pricing' },
+  { text: 'About', link: '/about' }
+];
 
 const Navbar = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-      }}
-    >
+    <Box sx={{ display: 'flex' }}>
       <AppBar>
         <Toolbar>
-          <MovieFilter sx={{ display: "flex", mr: 1 }} />
+          <MovieFilter sx={{ display: 'flex', mr: 1 }} />
           <Typography
             variant="h6"
-            component="div"
             sx={{
               flexGrow: 1,
-              display: "block",
-              fontFamily: "monospace",
+              display: 'block',
+              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: ".3rem",
+              letterSpacing: '.3rem',
             }}
           >
-            sudomovie
+            <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to="/">
+              Sudomovies
+            </Link>
           </Typography>
-
-          <Box sx={{ display: "block" }}>
+          <Box sx={{ display: 'block' }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
+              <NavLink
+                to={item.link}
+                key={item.text}
+                className={({ isActive }) => isActive ? 'nav-active' : 'nav-inactive'}
+              >
+                {item.text}
+              </NavLink>
             ))}
           </Box>
         </Toolbar>
       </AppBar>
-    </Box>
+    </Box >
   );
-};
+}
 
 export default Navbar;
