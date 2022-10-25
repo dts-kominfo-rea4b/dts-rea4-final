98c7e2c95fc99fa864061cb10c0270b8ea0ac14b
zainahda <zainahda@gmail.com>
2022-10-25 17:48:37 +0700

    Refactoring react router path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import NavbarComp from './components/NavbarComp';
-import SliderComp from './components/SliderComp';
-import api from "../src/api/Api";
-import CardComp from './components/CardComp';
-import CardRankComp from './components/CardRankComp';
-import Footer from './components/Footer';
+import React from "react";
+import NavbarComp from "./components/NavbarComp";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import MovieList from "./pages/MovieList";
+import Search from "./pages/Search";
+import api from "./api/Api";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 const App = () => {
   return (
     <div>
-      <NavbarComp/>
-      <SliderComp url={api.fetchTrending}/>
-      <CardComp title="Now Playing" url={api.fetchNowPlaying}/>
-      <CardComp title="Popular" url={api.fetchTrending}/>
-      <CardRankComp title="Up Coming" url={api.fetchUpComing}/>
-      <Footer />
+      <Router>
+        <NavbarComp />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<MovieList url={api.fetchUpComing} />} />
+          <Route path="/search" element={<Search url={api.fetchAll}  searchUrl={api.fetchSearch}/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
