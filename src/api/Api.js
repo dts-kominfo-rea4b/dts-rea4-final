5fe46c1d3cf56720a7ba7f39552251d6da873e93
zainahda <zainahda@gmail.com>
2022-10-25 17:49:50 +0700

    adding path search and all movies

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,7 +4,11 @@ const request = {
     fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
     fetchNowPlaying: `/movie/now_playing?api_key=${API_KEY}`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
-    fetchUpComing: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchUpComing: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=`,
+    fetchSearch: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=/`,
+    fetchAll:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 }
+
+
 
 export default request;
